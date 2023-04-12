b4fbce067851ea234d6233704c183e302040a17e
AmjustGettingStarted <gamerhmv2050@gmail.com>
2023-04-12 13:26:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$.ajax({
+  type: "get",
+  url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian",
+  success: function (response) {
+    console.log(response.meals);
+
+    for (var i = 0; i < response.meals.length; i++) {
+      var newitem = `<div class='col-md-3 '>
+        <p>${response.meals[i].strMeal}</p>
+        <img src=${response.meals[i].strMealThumb} class='img-fluid'/> 
+        <p>${response.meals[i].idMeal}</p>
+        </div>`;
+
+      $("#myitems").append(newitem);
+    }
+  },
+  error: function (error) {
+    console.log(error);
+  },
+});
